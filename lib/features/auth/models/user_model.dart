e2d3634fc853b15def4fcec8cdd8396b19f72790
gh-joshua <limguanhua00@gmail.com>
2026-04-09 18:36:29 +0800

    Auto-commit: user_model.dart

--- lib/features/auth/models/user_model.dart
+++ lib/features/auth/models/user_model.dart
@@ -3,16 +3,35 @@ class UserModel {
   final String id;
   final String email;
   final String? name;
+  final String? gender;
+  final DateTime? dateOfBirth;
 
-  UserModel({required this.id, required this.email, this.name});
+  UserModel({
+    required this.id,
+    required this.email,
+    this.name,
+    this.gender,
+    this.dateOfBirth,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       id: json['id'] as String,
       email: json['email'] as String,
       name: json['name'] as String?,
+      gender: json['gender'] as String?,
+      dateOfBirth: json['dateOfBirth'] != null
+          ? DateTime.tryParse(json['dateOfBirth'] as String)
+          : null,
     );
   }
 
-  Map<String, dynamic> toJson() => {'id': id, 'email': email, 'name': name};
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'email': email,
+        'name': name,
+        'gender': gender,
+        'dateOfBirth':
+            dateOfBirth != null ? dateOfBirth!.toIso8601String().split('T').first : null,
+      };
 }
